9f72ed0ac73ae49165b6a0406a59715b0efe2acb
Johan Steen <artstorm@gmail.com>
2013-04-04 17:05:47 +0700

    Implements catching of humans.txt url

--- lib/WPHumansTxt/Generate.php
+++ lib/WPHumansTxt/Generate.php
@@ -3,8 +3,25 @@
  * Generate a virtual humans.txt.
  *
  * @author   Johan Steen <artstorm at gmail dot com>
- * @uri      http://johansteen.se/
+ * @link     http://johansteen.se/
  */
 class WPHumansTxt_Generate
 {
+    public function __construct() {
+        add_action('template_redirect', array(&$this, 'redirect'), 1);
+    }
+
+    public function redirect()
+    {
+        $currentUrl = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $humanUrl = str_replace(
+            array('http://', 'https://'),
+            '',
+            home_url('humans.txt')
+        );
+        if ($humanUrl == $currentUrl) {
+            var_dump('here');
+            die;
+        }
+    }
 }
