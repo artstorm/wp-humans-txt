5dacb0cb1fafbde313eb917d2667d393a28124b7
Johan Steen <artstorm@gmail.com>
2013-04-04 15:07:22 +0700

    Cleans up unit test.

--- tests/TestWPHumanTxt.php
+++ tests/TestWPHumanTxt.php
@@ -4,13 +4,10 @@ class TestWPHumanTxt extends WP_UnitTestCase {
 
     private $plugin;
 
-
     public function setUp()
     {
         parent::setUp();
-        // $this->plugin = $GLOBALS['wp-humans-txt'];
-        // file_put_contents('D:\Dropbox\Code\WordPress\wp-humans-txt\tests\filename.txt', print_r($GLOBALS, true));
-        // $this->plugin = WPHumansTxt::getInstance();
+        $this->plugin = WPHumansTxt::getInstance();
     }
 
     // -------------------------------------------------------------------------
@@ -19,18 +16,12 @@ class TestWPHumanTxt extends WP_UnitTestCase {
 
     public function testPluginInitialization()
     {  
-        // var_dump(PostSnippets);
-        // $test = PostSnippets::getInstance();
-        $this->plugin = WPHumansTxt::getInstance();
-        // $this->assertFalse(null == $this->plugin);  
+        $this->assertFalse(null == $this->plugin);
     }
 
     public function testMethod()
     {
-        $test = WPHumansTxt::getInstance();
-        var_dump($test);
-        $test = $test->testing();
-        var_dump($test);
+        $test = $this->plugin->testing();
         $this->assertEquals('hello world', $test);
     }
 
