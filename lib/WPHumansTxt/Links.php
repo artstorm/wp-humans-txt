af290b36dd24ded563b039643948ad7e2da02985
Johan Steen <artstorm@gmail.com>
2013-04-16 15:23:24 +0700

    Adds display of the button in the wp footer.

--- lib/WPHumansTxt/Links.php
+++ lib/WPHumansTxt/Links.php
@@ -9,14 +9,27 @@ class WPHumansTxt_Links
 {
     public function __construct()
     {
-        add_action('wp_head', array(&$this, 'wpHead'));
+        add_action('wp_head', array(&$this, 'head'));
+        add_action('wp_footer', array(&$this, 'footer'));
     }
 
-    public function wpHead()
+    public function head()
     {
         $options = get_option(WPHumansTxt::OPTION_KEY);
         if ($options['author_link']) {
             printf("<link rel='author' href='%s' />\n", home_url('humans.txt'));
+        }
+    }
+
+    public function footer()
+    {
+        $options = get_option(WPHumansTxt::OPTION_KEY);
+        if ($options['button']) {
+            printf(
+                "<a href='%s'><img src='%s' /></a>",
+                home_url('humans.txt'),
+                plugins_url('assets/'.$options['button'], WPHumansTxt::FILE)
+            );
         }
     }
 }
