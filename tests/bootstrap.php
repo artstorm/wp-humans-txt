55e29468f7b65f1310d3629c62f0ada0d0c6c7ef
Johan Steen <artstorm@gmail.com>
2015-12-15 09:08:33 +0700

    Adds additional StyleCI adjustments

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 $_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) {
+if (!$_tests_dir) {
     $_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
@@ -9,7 +9,7 @@ require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin()
 {
-	require dirname(__FILE__) . '/../wp-humans-txt.php';
+    require dirname(__FILE__) . '/../wp-humans-txt.php';
 }
 tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,10 +2,9 @@
 
 class SampleTest extends WP_UnitTestCase
 {
-	public function testSample()
+    public function testSample()
     {
-		// replace this with some actual testing code
-		$this->assertTrue(true);
-	}
+        // replace this with some actual testing code
+        $this->assertTrue(true);
+    }
 }
-
