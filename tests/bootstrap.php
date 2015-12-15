1b66b5762202f90476f97940347324c729dc3e54
Johan Steen <artstorm@gmail.com>
2015-12-15 15:10:13 +0700

    Adjusts plugin slug to comply with repository slug

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin()
 {
-    require dirname(__FILE__) . '/../wp-humans-txt.php';
+    require dirname(__FILE__) . '/../wp-humanstxt.php';
 }
 tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
