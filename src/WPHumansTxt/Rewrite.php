1db2e323335166071401bb55dacd8994271f62cd
Johan Steen <artstorm@gmail.com>
2015-12-17 14:59:05 +0700

    Implements custom template tag handling
    Also implements initial template tag to output Wordpress with version
    number.

--- src/WPHumansTxt/Rewrite.php
+++ src/WPHumansTxt/Rewrite.php
@@ -7,11 +7,19 @@
  */
 class WPHumansTxt_Rewrite
 {
+    /**
+     * Initialize humans.txt output
+     */
     public function __construct()
     {
         add_action('template_redirect', array(&$this, 'redirect'), 1);
     }
 
+    /**
+     * Handle the redirect to use custom output.
+     *
+     * @return void
+     */
     public function redirect()
     {
         $currentUrl = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
@@ -25,8 +33,45 @@ class WPHumansTxt_Rewrite
             $options = get_option(WPHumansTxt::OPTION_KEY);
 
             header('Content-Type: text/plain; charset=utf-8');
-            echo $options['humanstxt'];
+
+            echo $this->replaceTags($options['humanstxt']);
             die;
         }
+    }
+
+    /**
+     * Find and replace tags in the text.
+     *
+     * @param  string $text
+     *
+     * @return string
+     */
+    protected function replaceTags($text)
+    {
+        $text = preg_replace_callback(
+            '|\[(.*?)\]|',
+            function ($matches)
+            {
+                $method = 'replace'.ucfirst($matches[1]);
+                if (method_exists($this, $method)) {
+                    return $this->$method();
+                }
+
+                return $matches[0];
+            },
+            $text
+        );
+
+        return $text;
+    }
+
+    /**
+     * Get WordPress with version number.
+     *
+     * @return string
+     */
+    protected function replaceWordpress()
+    {
+        return 'WordPress '.get_bloginfo('version');
     }
 }
